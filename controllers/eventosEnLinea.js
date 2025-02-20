57ee66ce749e9e556fd0bc943732561875af68f7
DarwinGmez <firemarlon2311@gmail.com>
2025-02-20 17:15:15 -0500

    2

--- controllers/eventosEnLinea.js
+++ controllers/eventosEnLinea.js
@@ -29,13 +29,39 @@ static async inscribirEvento(req, res) {
         if (evento.length === 0) {
         alert("Evento creado")
         }
-
         if (evento[0].registrante.length >= evento[0].capacity) {
             alert("esta lleno")
         }
-    
-
 }
+
+static async inscribirUsuario(req, res) 
+{
+    let { eventoid, usuario_id } = req.body;
+    try {
+        const [evento] = await db.execute("SELECT * FROM eventos WHERE id = ?", [eventoid]);
+        if (evento[0].capacidad_max > 0) {
+            await db.query("insert into inscripciones (evento_id, usuario_id) VALUES (?, ?)", [eventoid, usuarioid]);
+            await db.query("update eventos set capacidad_max = capacidad_max - 1 WHERE id = ?", [eventoid]);
+            res.json({ mensaje: "exitoso" });
+        }
+    } catch (error) {
+    console.log("error")
+    }
+}
+
+static async cancelarInscripcion(req, res) 
+{
+    let { eventoid, usuarioid } = req.body;
+    try {
+        await db.query("delete from  inscripciones where evento_id = ? and usuario_id = ?", [eventoid, usuarioid]);
+        await db.query("update eventos set capacidad_max = capacidad_max + 1 where id = ?", [eventoid]);
+        res.json({ mensaje: "la incripcion fue cancelada" });
+    } catch (error) {
+        console.log("error")
+    }
+}
+
+
 }
 
 
